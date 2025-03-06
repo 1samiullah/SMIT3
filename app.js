7e7a5f1a50dd5be9bfcbbcb7f4b3fb3a2a8725ad
Sami ullah <samiullahyounus10@gmail.com>
2025-03-06 08:22:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+// let inputValue = document.getElementById("input");
+// let currentValue = "";
+// let opr1 = "";
+// function num(number){
+
+//   currentValue += number
+
+//   inputValue.value = currentValue;
+//   console.log(inputValue);
+  
+
+// }
+
+// function opt(oprater) {
+  
+//   inputValue.value += oprater
+
+//   console.log(inputValue);
+  
+    
+
+ 
+  
+  
+
+// }
+
+// function calculator() {
+
+//   console.log(inputValue.value);
+  
+// }
+let inputValue = document.getElementById("input");
+function calInput(a){
+    
+}
